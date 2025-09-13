eddef000056167a394a136945b162729c6280b3e
Rachel Davez <racheld9999@outlook.com>
2025-09-13 18:57:10 +0200

    feat: update static file settings and adjust Whitenoise version in requirements

--- codestar/settings.py
+++ codestar/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
