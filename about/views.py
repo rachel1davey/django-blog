11e007bfaf40de93711e02e841a8db63e560a0d6
Rachel Davez <racheld9999@outlook.com>
2025-09-16 12:01:17 +0200

    fix: correct form handling logic in about_me view for CollaborateForm submission

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About, CollaborateRequest
 from .forms import CollaborateForm
+from django.contrib import messages
 
 
 def about_me(request):
@@ -9,6 +10,14 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     form = CollaborateForm()
+
+    if request.method == "POST":
+        form = CollaborateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval')
 
     return render(
         request,
